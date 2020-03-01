9d57587e7f7c9b7a5e2e9ff0316743603ee2d904
zhaopengfei <1027926875@qq.com>
2020-03-01 11:18:29 +0800

    add nettyServer

---
+++ src/main/java/com/eru/netty/NettyServer.java
@@ -0,0 +1,53 @@
+package com.eru.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.util.Attribute;
+import io.netty.util.AttributeKey;
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.GenericFutureListener;
+
+/**
+ * Created by eru on 2020/3/1.
+ */
+public class NettyServer {
+
+    private static final int BEGIN_PORT = 8000;
+
+    public static void main(String[] args) {
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workGroup = new NioEventLoopGroup();
+
+        final ServerBootstrap bootstrap = new ServerBootstrap();
+        final AttributeKey<Object> clientKey = AttributeKey.newInstance("clientKey");
+        bootstrap.group(bossGroup, workGroup)
+                .channel(NioServerSocketChannel.class)
+                .attr(AttributeKey.newInstance("serverName"), "nettyServer")
+                .childAttr(clientKey, "clientValue")
+                .childOption(ChannelOption.SO_KEEPALIVE, true)
+                .childOption(ChannelOption.TCP_NODELAY, true)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel ch) {
+                        System.out.println(ch.attr(clientKey).get());
+                    }
+                });
+        bind(bootstrap, 3306);
+    }
+
+    public static void bind(ServerBootstrap bootstrap, final int port){
+        bootstrap.bind(port).addListener(future -> {
+            if (future.isSuccess()){
+                System.out.println("端口绑定成功");
+            }else {
+                System.out.println("端口绑定失败");
+                bind(bootstrap, port + 1);
+            }
+        });
+    }
+
+}
